166224cc03c37241536cf165847269b5e3a334a8
imok1948 <imok1948@gmail.com>
2022-04-22 16:36:04 +0530

    Moved normal codes to methods so that we can easily understand

--- app/src/main/java/com/example/expensemanager/fragments/SecondFragment.java
+++ app/src/main/java/com/example/expensemanager/fragments/SecondFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
 import android.widget.ImageView;
@@ -20,6 +19,7 @@ import androidx.fragment.app.Fragment;
 import com.example.expensemanager.R;
 import com.example.expensemanager.databinding.FragmentSecondBinding;
 import com.example.expensemanager.utils.CategoryType;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
 import java.util.LinkedList;
@@ -29,51 +29,102 @@ public class SecondFragment extends Fragment {
 
   private static final int CATEGORIES_PER_ROW = 4;
   private FragmentSecondBinding binding;
+  private View rootView;
   private Calendar defaultCalendar;
-  private View viewCalendarDialog;
+  private View viewOfCalendarDialog;
   private AlertDialog calendarDialog, categoryDialog;
-  private View viewCategoryDialog;
-  private LinearLayout viewAddActivityDialogRow;
-  private LinearLayout viewAddExpenseDialogCategory;
-
-
+  private View viewOfCategoryDialog;
+  private LinearLayout viewOfAddExpenseDialogRow;
   private View.OnClickListener dialogButtonClickListener;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentSecondBinding.inflate(inflater, container, false);
-    return binding.getRoot();
+    rootView = binding.getRoot();
+    return rootView;
   }
-
 
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    initiate();
+    assignments();
     setupListeners();
-
+    initiate();
   }
 
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
 
   private void initiate() {
 
-    dialogButtonClickListener = new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Toast.makeText(getContext(), "Selected : " + view.getTag(), Toast.LENGTH_SHORT).show();
-      }
-    };
+    initDialogs();
 
-
-    defaultCalendar = Calendar.getInstance();
+    //Now setting up focus and keyboard for the amount view
     binding.edittextAddTransactionAmount.requestFocus();
-    setupListeners();
     InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
     imm.showSoftInput(binding.edittextAddTransactionAmount, 0);
-    initDialogs();
   }
 
+  private void initDialogs() {
+    createDateDialog();
+    createCategoryDialog();
+  }
 
-  private LinearLayout getInflatedLinearLayout(int id, Object tag, View.OnClickListener onClickListener, ViewGroup parent) {
+  //Done and working fine
+  private void createCategoryDialog() {
+    AlertDialog.Builder categoryDialogBuilder = new AlertDialog.Builder(getActivity());
+
+    //Find category box
+    LinearLayout linearLayoutBox = viewOfCategoryDialog.findViewById(R.id.dialog_category_picker_linear_layout_box);
+    int totalCategories = 14;
+    List<CategoryType> categories = getDummyCategories(totalCategories);
+    int totalRows = (int) Math.ceil(totalCategories / (float) CATEGORIES_PER_ROW);
+    for (int i = 0; i < totalRows; i++) {
+      viewOfAddExpenseDialogRow = getCategoryDialogRow(R.layout.add_activity_dialog_row);
+      for (int j = 0; j < CATEGORIES_PER_ROW && categories.size() > 0; j++) {
+        viewOfAddExpenseDialogRow.addView(getLayoutForCategoryDialog(R.layout.add_expense_dialog_category, categories.get(0), dialogButtonClickListener, viewOfAddExpenseDialogRow));
+        categories.remove(0);
+      }
+      linearLayoutBox.addView(viewOfAddExpenseDialogRow);
+    }
+    categoryDialogBuilder.setView(viewOfCategoryDialog);
+    categoryDialog = categoryDialogBuilder.create();
+  }
+
+  private void createDateDialog() {
+    AlertDialog.Builder calendarBuilder = new AlertDialog.Builder(getActivity());
+
+    DatePicker datePicker = viewOfCalendarDialog.findViewById(R.id.datepicker_add_transactions_calendar);
+    datePicker.init(defaultCalendar.get(Calendar.YEAR), defaultCalendar.get(Calendar.MONTH), defaultCalendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+      @Override
+      public void onDateChanged(DatePicker datePicker1, int year, int month, int dayOfMonth) {
+        setExpenseDate(year, month, dayOfMonth);
+        calendarDialog.dismiss();
+      }
+    });
+
+    calendarBuilder.setView(viewOfCalendarDialog);
+    calendarDialog = calendarBuilder.create();
+  }
+
+  private void setExpenseDate(int year, int month, int dayOfMonth) {
+    //Month is 0 based
+    String[] monthStrings = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    binding.textviewAddTransactionDate.setText(dayOfMonth + " " + monthStrings[month] + " " + year + ",");
+  }
+
+  private void assignments() {
+    //Initialize with their respective views
+    viewOfCalendarDialog = getLayoutInflater().inflate(R.layout.dialog_select_date, null);
+    viewOfCategoryDialog = getLayoutInflater().inflate(R.layout.dialog_category_picker, null);
+    defaultCalendar = Calendar.getInstance();
+    defaultCalendar.setTimeInMillis(System.currentTimeMillis());
+
+  }
+
+  private LinearLayout getLayoutForCategoryDialog(int id, Object tag, View.OnClickListener onClickListener, ViewGroup parent) {
     CategoryType categoryType = (CategoryType) tag;
     LinearLayout linearLayout = (LinearLayout) getLayoutInflater().inflate(id, parent, false);
     linearLayout.setTag(tag);
@@ -83,79 +134,24 @@ public class SecondFragment extends Fragment {
     return linearLayout;
   }
 
-
   private LinearLayout getCategoryDialogRow(int viewId) {
     return (LinearLayout) getLayoutInflater().inflate(R.layout.add_activity_dialog_row, null);
   }
 
-
-  private void initDialogs() {
-    //Initialize with their respective views
-    viewCalendarDialog = getLayoutInflater().inflate(R.layout.dialog_select_date, null);
-    viewCategoryDialog = getLayoutInflater().inflate(R.layout.dialog_category_picker, null);
-    viewAddActivityDialogRow = null;
-
-
-    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-    builder.setTitle("Select Date");
-
-    DatePicker datePicker = viewCalendarDialog.findViewById(R.id.datepicker_add_transactions_calendar);
-    Calendar calendar = Calendar.getInstance();
-    calendar.setTimeInMillis(System.currentTimeMillis());
-
-    datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
-      @Override
-      public void onDateChanged(DatePicker datePicker1, int year, int month, int dayOfMonth) {
-        Toast.makeText(getContext(), "Date selected " + datePicker1.getDayOfMonth(), Toast.LENGTH_SHORT).show();
-      }
-    });
-    builder.setView(viewCalendarDialog);
-    calendarDialog = builder.create();
-
-
-    AlertDialog.Builder categoryDialogBuilder = new AlertDialog.Builder(getActivity());
-
-    //Find box and add
-    LinearLayout linearLayoutBox = viewCategoryDialog.findViewById(R.id.dialog_category_picker_linear_layout_box);
-
-
-    int totalCategories = 14;
-    List<CategoryType> categories = getDummyCategories(totalCategories);
-
-    int totalRows = (int) Math.ceil(totalCategories / (float) CATEGORIES_PER_ROW);
-
-    for (int i = 0; i < totalRows; i++) {
-      viewAddActivityDialogRow = getCategoryDialogRow(R.layout.add_activity_dialog_row);
-      for (int j = 0; j < CATEGORIES_PER_ROW && categories.size() > 0; j++) {
-        viewAddActivityDialogRow.addView(getInflatedLinearLayout(R.layout.add_expense_dialog_category, categories.get(0), dialogButtonClickListener, viewAddActivityDialogRow));
-        categories.remove(0);
-      }
-      linearLayoutBox.addView(viewAddActivityDialogRow);
-    }
-
-    categoryDialogBuilder.setView(viewCategoryDialog);
-    categoryDialog = categoryDialogBuilder.create();
-  }
-
   private void setupListeners() {
+    dialogButtonClickListener = new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(getContext(), "Selected category : " + view.getTag(), Toast.LENGTH_SHORT).show();
+      }
+    };
     binding.linearlayoutAddTransactionCategory.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         categoryDialog.show();
       }
     });
-
     //ToDo : Currently the cursor on clicking the Layout after edittext doesn't bring it to end of edittext, do it
-    binding.textviewAddTransactionAmount.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        binding.edittextAddTransactionAmount.requestFocus();
-        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-
-        imm.showSoftInput(binding.edittextAddTransactionAmount, 0);
-      }
-    });
     binding.linearlayoutAddTransactionAmount.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
@@ -164,7 +160,7 @@ public class SecondFragment extends Fragment {
         imm.showSoftInput(binding.edittextAddTransactionAmount, 0);
       }
     });
-    binding.buttonAddTransactionAmountDate.setOnClickListener(new View.OnClickListener() {
+    binding.linearlayoutAddTransactionDate.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         calendarDialog.show();
@@ -173,18 +169,10 @@ public class SecondFragment extends Fragment {
     binding.linearlayoutAddTransactionPayment.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Toast.makeText(getContext(), "Hi, I am linear layout", Toast.LENGTH_SHORT).show();
+        Snackbar.make(rootView, "I will show you available accounts", Snackbar.LENGTH_SHORT).show();
       }
     });
-
   }
-
-  @Override
-  public void onDestroyView() {
-    super.onDestroyView();
-    binding = null;
-  }
-
 
   private List<CategoryType> getDummyCategories(int n) {
     String[] categoryNames = new String[]{"Foods", "Cloths", "Health", "Electronics", "Miscllenciuos", "Study", "Work", "Others"};
